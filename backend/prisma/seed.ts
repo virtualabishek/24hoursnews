73e2623758d9495968728df937f0d8db26ffaca4
virtualabishek <717abishekneupane@gmail.com>
2025-09-18 14:43:43 +0545

    add: database seeding added

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -6,15 +6,37 @@ const prisma = new PrismaClient();
 const publisherData = [
   {
     name: "Ratopati",
+    logoUrl:
+      "https://play-lh.googleusercontent.com/EK7tpygXYxon7v8hLdOjvfL4M5X7ZeERzoxwHhvF5iQ-Wvo5mCCnrzjeQipm67GGrcM",
   },
   {
-    name: "Onlinekhabar",
+    name: "Online Khabar",
+    logoUrl:
+      "https://play-lh.googleusercontent.com/7fQNxv7HXXzN8SivPrNCxvK6Wii_VcKmvyGUvXDlGumOaBOxeFPOAk1zZ2BrWzW3TQ",
   },
   {
     name: "TechPana",
+    logoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSajaOamjFDRxjfpk1gykdD5Y2UZYsM2Bm_Fg&s",
   },
   {
     name: "BBC Nepali",
+    logoUrl:
+      "https://yt3.googleusercontent.com/ggl56rqMnWuNC7741kzL6qx_Oyl9gLp3YLWEZQ5Ppqc8wjdX5pDVpYGRq6y85piHuF1pjF8B=s900-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    name: "Setopati",
+    logoUrl: "https://www.setopati.com/themes/setopati/images/og-image.jpg",
+  },
+  {
+    name: "Rajdhani Daily",
+    logoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvhMD49lqpkIT_6d-UJ3mqN31wMAoVPjgERw&s",
+  },
+  {
+    name: "Himalayan Times",
+    logoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnIZ9wYAJ-2ZblcXNiyyZ7zoYSqoJHm4_iw&s",
   },
 ];
 
@@ -27,6 +49,7 @@ async function main() {
       update: {},
       create: {
         name: p.name,
+        logoUrl: p.logoUrl,
       },
     });
     console.log(`Created or found publisher: ${publisher.name}`);
